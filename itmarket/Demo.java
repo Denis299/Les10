98049eb8f4e7435f9776fc38a8d2436855aba71c
Denis299 <79289053+Denis299@users.noreply.github.com>
2021-03-03 07:07:16 +0200

    Update Demo.java

--- itmarket/Demo.java
+++ itmarket/Demo.java
@@ -5,43 +5,26 @@ public class Demo {
 
         Customer client1 = new Customer("Тест","Киев","Женский");
         Customer client2 = new Customer("Дима","Львов","Мужской");
-        ElectronicOrder z1 = new ElectronicOrder("",null,"Киев","Львов",190,client1);
-        ElectronicOrder z2 = new ElectronicOrder("",null,"Tokyo","Paris",90,client2);
-        FurnitureOrder z3 = new FurnitureOrder("",null,"Киев","",600,client2);
-        FurnitureOrder z4 = new FurnitureOrder("",null,"","",400,client2);
-        System.out.println("Name "+z1.customerOwned.getName());
-        z1.validateOrder();
-        z1.calculatePrice();
-        System.out.println("Total "+z1.totalPrice);
-        System.out.println(z1.dateChipped);
-        z1.confirmChipping();
-        System.out.println(z1.dateChipped);
-
-        System.out.println(" z2 ");
-        z2.validateOrder();
-        z2.calculatePrice();
-        System.out.println("Total "+z2.totalPrice);
-        System.out.println(z2.dateChipped);
-        z2.confirmChipping();
-        System.out.println(z2.dateChipped);
-
-        System.out.println(" z3 ");
-        z3.validateOrder();
-        z3.calculatePrice();
-        System.out.println("Total "+z3.totalPrice);
-        System.out.println(z3.dateChipped);
-        z3.confirmChipping();
-        System.out.println(z3.dateChipped);
-
-
-        System.out.println(" z4 ");
-        z4.validateOrder();
-        z4.calculatePrice();
-        System.out.println("Total "+z4.totalPrice);
-        System.out.println(z4.dateChipped);
-        z4.confirmChipping();
-        System.out.println(z4.dateChipped);
-
-
+        ElectronicOrder z1 = new ElectronicOrder("Заказ1",null,"Киев","Львов",190,client1);
+        ElectronicOrder z2 = new ElectronicOrder("Заказ2",null,"Tokyo","Paris",90,client2);
+        FurnitureOrder z3 = new FurnitureOrder("Заказ3",null,"Киев","Луцк",600,client2);
+        FurnitureOrder z4 = new FurnitureOrder("Заказ4",null,"Одесса","Харьков",400,client2);
+         tests(z1);
+         tests(z2);
+         tests(z3);
+         tests(z4);
     }
+ 
+    static void tests(Order order){
+        System.out.println(order.customerOwned.getName());
+        System.out.println(order.itemName);
+        order.validateOrder();
+        order.calculatePrice();
+        System.out.println("Total "+order.totalPrice);
+        System.out.println(order.dateChipped);
+        order.confirmChipping();
+        System.out.println(order.dateChipped);
+
+    };
 }
+
